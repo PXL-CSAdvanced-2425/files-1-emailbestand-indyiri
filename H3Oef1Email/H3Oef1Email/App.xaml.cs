5195fa53104c5e050ec3276e78dde1efb63e4063
Indy Iri <indy.iri@student.pxl.be>
2025-03-03 13:42:28 +0100

    wpf aangemaakt

---
+++ H3Oef1Email/H3Oef1Email/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H3Oef1Email;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
